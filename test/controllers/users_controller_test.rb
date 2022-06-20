01da2b5b88dc6bb65790cb2ca5b2ee61fc1659d5
coderdsraj001 <csdsraj2050@gmail.com>
2022-06-20 16:51:29 +0530

    Add user following

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,4 @@
-  require "test_helper"
+require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
@@ -7,14 +7,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @other_user = users(:archer)
   end
 
-  test "should get new" do
-    get signup_path
-    assert_response :success
-  end
-
   test "should redirect index when not logged in" do
     get users_path
     assert_redirected_to login_url
+  end
+
+  test "should get new" do
+    get signup_path
+    assert_response :success
   end
 
   test "should redirect edit when not logged in" do
@@ -25,7 +25,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should redirect update when not logged in" do
     patch user_path(@user), params: { user: { name: @user.name,
-                                              email: @user.email }}
+                                              email: @user.email } }
     assert_not flash.empty?
     assert_redirected_to login_url
   end
@@ -40,7 +40,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should redirect update when logged in as wrong user" do
     log_in_as(@other_user)
     patch user_path(@user), params: { user: { name: @user.name,
-                                              email: @user.email }}
+                                              email: @user.email } }
     assert flash.empty?
     assert_redirected_to root_url
   end
@@ -60,4 +60,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
   end
 
+  test "should redirect following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
+  end
 end
--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -3,7 +3,6 @@ require "test_helper"
 class MicropostTest < ActiveSupport::TestCase
   def setup
     @user = users(:michael)
-    # This code is not idiomatically correct.
     @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
